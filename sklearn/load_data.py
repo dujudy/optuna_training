6879a2df328449a1ea430d13eaecab68ff334747
dujudy <judytdu@gmail.com>
2021-07-12 08:26:00 -0400

    Optuna with Scikit learn. Gradient Boosting scored with AUC ROC currently functional.

---
+++ sklearn/load_data.py
@@ -0,0 +1,77 @@
+from config import *
+import pandas as pd
+import numpy as np
+
+def process_data(path, features_start, exclude):
+ input_data = pd.read_csv(path, header = 0, sep='\t', comment='#').dropna().reset_index(drop = True)
+ input_data = input_data[input_data["protein_id"].isin(exclude) == False]
+ input_data['label'] = input_data["label"].replace("positives", 1).replace("negatives", 0)
+ features = input_data.iloc[:,features_start:(len(input_data.columns))].to_numpy()
+ labels = np.array([1 if lbl == 'positives' else 0 for lbl in input_data['label'].tolist()])
+ return input_data, features, labels
+
+## initialize data dicts
+features = {"ref":{}, "mut":{}, "abs":{}, "absref":{}, "absmut":{}, "mutref":{}};
+labels = {"ref":{}, "mut":{}, "abs":{}, "absref":{}, "absmut":{}, "mutref":{}};
+input_df = {"ref":{}, "mut":{}, "abs":{}, "absref":{}, "absmut":{}, "mutref":{}};
+metadata = {"ref":{}, "mut":{}, "abs":{}, "absref":{}, "absmut":{}, "mutref":{}};
+feature_columns = {"ref":{}, "mut":{}, "abs":{}, "absref":{}, "absmut":{}, "mutref":{}};
+feature_columns["ref"] = cols
+
+for key in ref_paths.keys():
+    print(key)
+    # reading reference + mutant data
+    if key in ["ptenDMS", "mcf10A"]:
+        input_df["ref"][key], features["ref"][key], labels["ref"][key] = process_data(root + ref_paths[key], start[key], [])
+        input_df["mut"][key], features["mut"][key], labels["mut"][key] = process_data(root + mut_paths[key], start[key], [])
+    elif key in ["maveDB"]:
+        input_df["ref"][key], features["ref"][key], labels["ref"][key] = process_data(root + ref_paths[key], start[key], exclude_maveDB)
+        input_df["mut"][key], features["mut"][key], labels["mut"][key] = process_data(root + mut_paths[key], start[key], exclude_maveDB)
+    else:
+        input_df["ref"][key], features["ref"][key], labels["ref"][key] = process_data(root + ref_paths[key], start[key], exclude)
+        input_df["mut"][key], features["mut"][key], labels["mut"][key] = process_data(root + mut_paths[key], start[key], exclude)
+    metadata["ref"][key] = input_df["ref"][key].iloc[:,0:start[key]]
+    metadata["mut"][key] = input_df["mut"][key].iloc[:,0:start[key]]
+    metas = list(metadata["mut"][key].columns)
+    dim = input_df["ref"][key].shape[1]
+
+    # create mutref
+    mutref = input_df["ref"][key].copy().merge(input_df["mut"][key].copy(), on = metas, how = 'left', suffixes = ["", "_ref"]).dropna()
+    features["mutref"][key] = mutref.iloc[:,start[key]:mutref.shape[1]].to_numpy()
+    labels["mutref"][key] = np.array([1 if lbl == 'positives' else 0 for lbl in mutref['label'].tolist()])
+    metadata["mutref"][key] = mutref.iloc[:,0:start[key]]
+
+    # create abs
+    features["abs"][key] = mutref.copy()[cols] - mutref.copy()[refcols].to_numpy()
+    features["abs"][key] = features["abs"][key].abs().to_numpy()
+    labels["abs"][key] = np.array([1 if lbl == 'positives' else 0 for lbl in mutref['label'].tolist()])
+    metadata["abs"][key] = mutref.iloc[:,0:start[key]]
+
+    # create absref
+    abs = mutref.copy()
+    abs[cols] = features["abs"][key]  #replace mut data with abs data
+    abs = abs[metas + cols] #create df with only abs data + metadata
+    absref = abs.copy()
+    absref = absref.iloc[:,0:dim].merge(input_df["ref"][key].copy(), on = metas, how = 'left', suffixes = ["", "_ref"]).dropna()
+    features["absref"][key] = absref[cols + refcols].to_numpy()
+    labels["absref"][key] = np.array([1 if lbl == 'positives' else 0 for lbl in absref['label'].tolist()])
+    metadata["absref"][key] = absref.iloc[:,0:start[key]]
+
+    # create absmut
+    absmut = abs.copy()
+    absmut = absmut.iloc[:,0:dim].merge(input_df["mut"][key].copy(), on = metas, how = 'left', suffixes = ["", "_mut"]).dropna()
+    features["absmut"][key] = absmut[cols + mutcols].to_numpy()
+    labels["absmut"][key] = np.array([1 if lbl == 'positives' else 0 for lbl in absmut['label'].tolist()])
+    metadata["absmut"][key] = absmut.iloc[:,0:start[key]]
+    del mutref, absref, absmut
+
+### Combine split datasets (t or d)
+def combine_d(data_dict):
+    return np.concatenate((data_dict["d1"], data_dict["d2"]), axis=0)
+for f in features.keys():
+    features[f]["d"] = combine_d(features[f])
+    labels[f]["d"] = combine_d(labels[f])
+    metadata[f]["d"] = metadata[f]["d1"].append(metadata[f]["d2"])
+
+def load_data(dataset, tranformation):
+    return input_df[tranformation][dataset]
---
+++ sklearn/run_optuna_sklearn.py
@@ -0,0 +1,78 @@
+"""
+Optuna optimization of hyperparameters
+"""
+import optuna
+from load_data import *
+from sklearn_fns import *
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import roc_auc_score, precision_recall_curve, auc
+
+def score_model(parameters, train_feats, train_labs, test_feats, test_labs, metric):
+    # define model type
+    classifier = GradientBoostingClassifier(**parameters)
+
+    # train model
+    classifier.fit(train_feats, train_labs)
+
+    # generating prediction probs for test set
+    y_score = classifier.predict_proba(test_feats)[:,1]
+
+    # generate scoring metric
+    if metric == "ROC":
+        # calculate ROC AUC
+        metric = roc_auc_score(test_labs, y_score)
+    elif metric == "PR":
+        # calculate auprc
+        precision, recall, thresholds = precision_recall_curve(test_labs, y_score)
+        metric = auc(recall, precision)
+    else:
+        # calculate mean accuracy
+        metric = classifier.score(test_feats, test_labs)
+    return(metric)
+
+## Unaltered default params
+    #loss='deviance', learning_rate=0.1, subsample=1.0, criterion='friedman_mse', min_weight_fraction_leaf=0.0,
+    #min_impurity_split=None, max_leaf_nodes=None, validation_fraction=0.1, n_iter_no_change=None, tol=0.0001, ccp_alpha=0.0
+    #min_weight_fraction_leaf=0.0
+
+def objective(trial, train, test, type, feats, input_df, metric):
+    # define model
+    params = {
+        'max_depth': trial.suggest_int("max_depth", 1, 20),
+        "n_estimators": trial.suggest_int("n_estimators", 75, 300),
+        "min_samples_split": trial.suggest_int("min_samples_split", 2, 10),
+        "min_samples_leaf": trial.suggest_int("min_samples_leaf", 1, 10),
+        "min_impurity_decrease": trial.suggest_float("min_impurity_decrease", 0.0, 0.25),
+        "min_samples_leaf": trial.suggest_int("min_samples_leaf", 1, 10),  make min larger 1--> 5?
+        "random_state": 7
+    }
+    # train and evaluate models
+    fold_1_auc = score_model(params, feats[type][train], input_df[type][train]["label"], feats[type][test], input_df[type][test]["label"], metric)
+    fold_2_auc = score_model(params, feats[type][test], input_df[type][test]["label"], feats[type][train], input_df[type][train]["label"], metric)
+
+    return 0.5 * (fold_1_auc + fold_2_auc)
+
+
+def fill_objective(train, test, type, feats, labs, scoring_metric):
+  def filled_obj(trial):
+    return objective(trial, train, test, type, feats, labs, scoring_metric)
+  return filled_obj
+
+def main(feature_type, scoring_metric, n_trials = 200):
+    specified_objective = fill_objective("d1", "d2", feature_type, features, input_df, scoring_metric)
+    study = optuna.create_study(direction="maximize")
+    study.optimize(specified_objective)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Optuna optimization of hyperparameters.")
+    parser.add_argument("--scoring_metric", type=str, default= "ROC",
+                        help="Full path to directory with labeled examples. ROC, PR, accuracy.")
+    parser.add_argument("--feature_type", type=str, default= "ref",
+                        help="Mapping of aa representation between mutant and reference. ref.")
+
+    # Specification args
+    parser.add_argument("--test_size", type=float, default=0.1, help="Percentage of data to use in hold-out test set.")
+
+    args = parser.parse_args()
+    main(args.feature_type, args.scoring_metric, n_trials = 200)
