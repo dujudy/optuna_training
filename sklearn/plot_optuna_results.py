3c128386fcff523c49debb7ffd08e3dce1b3e5d5
dujudy <judytdu@gmail.com>
2021-07-15 15:03:53 -0400

    minor bugs in plotting script.

--- sklearn/plot_optuna_results.py
+++ sklearn/plot_optuna_results.py
@@ -4,14 +4,14 @@ from optuna.visualization import plot_param_importances, plot_parallel_coordinat
 def plot_optuna_results(study, study_name = "PRd1d2ref"):
     print(study)
     fig = plot_optimization_history(study)
-    fig.write_image("optina_optimizationhistory_"+ study_name + ".png")
+    fig.write_image(study_name + "_optina_optimizationhistory.png")
 
     fig = plot_parallel_coordinate(study)
-    fig.write_image("optuna_parallelcoordinate_"+ study_name + ".png")
+    fig.write_image(study_name + "_optuna_parallelcoordinate.png")
 
     fig = plot_slice(study)
-    fig.write_image("optuna_slice_"+ study_name + ".png")
+    fig.write_image(study_name + "_optuna_slice.png")
 
     fig = plot_param_importances(study)
-    fig.write_image("optuna_paramimportances_ "+ study_name + ".png")
+    fig.write_image(study_name + "_optuna_paramimportances.png")
     return(fig)
