1757b6e610307574d537a71ec8f562040d4d6ab7
Judy Du <judytdu@gmail.com>
2021-08-05 21:04:38 -0400

    Della-stable version of generate_prediction_probs

--- sklearn/generate_prediction_probs.py
+++ sklearn/generate_prediction_probs.py
@@ -8,13 +8,13 @@ Returns the corresponding prediction probabilities.
 """
 # testing: generate prediction probabilities
 
-def generate_prediction_probs(model, model_name, X, y, testing_pfix):
+def generate_prediction_probs(model, model_name,X, y, metas, testing_pfix):
   probs = model.predict_proba(X)
 
   # compile pred_probs data to save
   pp_dataframe = pd.DataFrame({"prediction_prob": probs[:,1], "label": y,
                                "models":np.repeat(model_name, probs.shape[0])})
-
+  pp_dataframe = pd.concat([metas[['protein_id', 'protein_position','reference_aa', 'mutant_aa', 'label']],pp_dataframe], axis = 1)  
   pp_dataframe.to_csv(path_or_buf = "./" + testing_pfix + "_PredictionProbs.csv")
   return(pp_dataframe)
 
@@ -42,4 +42,5 @@ if __name__ == "__main__":
             model = load(args.model_path)
             generate_prediction_probs(model, args.model_name,
                                       features[args.feature_type][data_name],
-                                      labels[args.feature_type][data_name], args.testing_pfix)
+                                      labels[args.feature_type][data_name],
+                                      metadata[args.feature_type][data_name], args.testing_pfix)
